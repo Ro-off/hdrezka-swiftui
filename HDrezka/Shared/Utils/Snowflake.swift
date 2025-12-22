1cb5d59b812ebf9edd0fff556b8731d35a660a52
voidboost <voidboost.hdrezka@gmail.com>
2025-12-22 17:28:01 +0200

    small improvements

--- HDrezka/Shared/Utils/Snowflake.swift
+++ HDrezka/Shared/Utils/Snowflake.swift
@@ -12,7 +12,7 @@ struct Snowflake: Hashable, Identifiable {
     private let noiseSeed: CGFloat
     private let rectCount: Int
     private let raysCount: Int
-    private let creationDate: Date
+    private let creationTime: TimeInterval
 
     init(id: UUID = .init(), startX: CGFloat, startY: CGFloat, startRotation: CGFloat, speed: CGFloat, rotationSpeed: CGFloat, opacity: CGFloat, size: CGFloat, noiseSeed: CGFloat, rectCount: Int, raysCount: Int) {
         self.id = id
@@ -26,32 +26,38 @@ struct Snowflake: Hashable, Identifiable {
         self.noiseSeed = noiseSeed
         self.rectCount = rectCount
         self.raysCount = raysCount
-        creationDate = .now
+        creationTime = Date.now.timeIntervalSinceReferenceDate
     }
 }
 
 extension Snowflake {
-    func draw(ctx: inout GraphicsContext, size: CGSize, date: Date, noise: Perlin) {
+    func draw(ctx: inout GraphicsContext, size: CGSize, time: TimeInterval, noise: Perlin) {
         guard let snowflakeSymbol = ctx.resolveSymbol(id: id) else { return }
 
-        let time = date.timeIntervalSince(creationDate)
+        let elapsed = time - creationTime
 
-        let x = startX + noise.getValue(noiseSeed + time * 0.1) * 30
-        let y = startY + speed * time
-        let rotation = startRotation + rotationSpeed * time
+        guard elapsed > 0 else { return }
 
-        let rangeX = size.width + self.size * 4
-        let rangeY = size.height + self.size * 4
+        let x = startX + noise.getValue(noiseSeed + elapsed * 0.1) * 30
+        let y = startY + speed * elapsed
+        let rotation = startRotation + rotationSpeed * elapsed
 
-        let truncatedX = x.truncatingRemainder(dividingBy: rangeX)
-        let posX = (truncatedX < 0 ? truncatedX + rangeX : truncatedX) - (self.size * 2)
-        let posY = y.truncatingRemainder(dividingBy: rangeY) - (self.size * 2)
+        let rangeX = size.width + radius * 2
+        let rangeY = size.height + radius * 2
 
-        ctx.drawLayer { ctx in
-            ctx.translateBy(x: posX, y: posY)
-            ctx.rotate(by: .degrees(rotation))
-            ctx.draw(snowflakeSymbol, at: .zero, anchor: .center)
-        }
+        let relativeX = x.truncatingRemainder(dividingBy: rangeX)
+        let posX = (relativeX < 0 ? relativeX + rangeX : relativeX) - radius
+        let posY = y.truncatingRemainder(dividingBy: rangeY) - radius
+
+        let transform = CGAffineTransform.identity
+            .translatedBy(x: posX, y: posY)
+            .rotated(by: Angle(degrees: rotation).radians)
+
+        ctx.concatenate(transform)
+
+        ctx.draw(snowflakeSymbol, at: .zero, anchor: .center)
+
+        ctx.concatenate(transform.inverted())
     }
 
     var radius: CGFloat {
@@ -69,36 +75,44 @@ extension Snowflake {
 extension Snowflake: View {
     var body: some View {
         Canvas(rendersAsynchronously: true) { ctx, size in
-            ctx.drawLayer { ctx in
-                ctx.translateBy(x: size.width / 2, y: size.height / 2)
+            let transform = CGAffineTransform.identity.translatedBy(x: size.width / 2, y: size.height / 2)
 
-                for ray in 0 ..< raysCount {
-                    ctx.drawLayer { ctx in
-                        ctx.rotate(by: .degrees(angleStep * CGFloat(ray)))
+            ctx.concatenate(transform)
 
-                        for rect in 0 ..< rectCount {
-                            let rectWidth = self.size * pow(0.9, CGFloat(rect) + 1)
+            for ray in 0 ..< raysCount {
+                let transform = CGAffineTransform.identity.rotated(by: Angle(degrees: angleStep * CGFloat(ray)).radians)
 
-                            ctx.drawLayer { ctx in
-                                ctx.translateBy(x: rectWidth * 0.7 * (CGFloat(rect) + 0.8), y: 0)
-                                ctx.rotate(by: .degrees(45))
-                                ctx.opacity = opacity
-                                ctx.fill(
-                                    Path(
-                                        CGRect(
-                                            x: -rectWidth / 2,
-                                            y: -rectWidth / 2,
-                                            width: rectWidth,
-                                            height: rectWidth,
-                                        ),
-                                    ),
-                                    with: .color(.teal),
-                                )
-                            }
-                        }
-                    }
+                ctx.concatenate(transform)
+
+                for rect in 0 ..< rectCount {
+                    let rectWidth = self.size * pow(0.9, CGFloat(rect) + 1)
+
+                    let transform = CGAffineTransform.identity
+                        .translatedBy(x: rectWidth * 0.7 * (CGFloat(rect) + 0.8), y: 0)
+                        .rotated(by: Angle(degrees: 45).radians)
+
+                    ctx.concatenate(transform)
+
+                    ctx.opacity = opacity
+                    ctx.fill(
+                        Path(
+                            CGRect(
+                                x: -rectWidth / 2,
+                                y: -rectWidth / 2,
+                                width: rectWidth,
+                                height: rectWidth,
+                            ),
+                        ),
+                        with: .color(.blue),
+                    )
+
+                    ctx.concatenate(transform.inverted())
                 }
+
+                ctx.concatenate(transform.inverted())
             }
+
+            ctx.concatenate(transform.inverted())
         }
         .frame(width: radius * 2, height: radius * 2)
         .tag(id)
--- HDrezka/iOS/Presentations/Components/SnowflakesView.swift
+++ HDrezka/iOS/Presentations/Components/SnowflakesView.swift
@@ -7,40 +7,42 @@ struct SnowflakesView: View {
 
     var body: some View {
         TimelineView(.animation) { timeline in
+            let currentTime = timeline.date.timeIntervalSinceReferenceDate
+
             Canvas(rendersAsynchronously: true) { ctx, size in
                 for snowflake in snowflakes {
-                    snowflake.draw(ctx: &ctx, size: size, date: timeline.date, noise: noise)
+                    snowflake.draw(ctx: &ctx, size: size, time: currentTime, noise: noise)
                 }
             } symbols: {
                 ForEach(snowflakes) { snowflake in
                     snowflake
                 }
             }
-            .onGeometryChange(for: CGSize.self) { geometry in
-                geometry.size
-            } action: { size in
-                let targetCount = Int((size.width * size.height) / 20000)
+        }
+        .onGeometryChange(for: CGSize.self) { geometry in
+            geometry.size
+        } action: { size in
+            let targetCount = Int((size.width * size.height) / 25000)
 
-                guard targetCount != snowflakes.count else { return }
+            guard targetCount != snowflakes.count else { return }
 
-                if snowflakes.count > targetCount {
-                    snowflakes.removeLast(snowflakes.count - targetCount)
-                } else if snowflakes.count < targetCount {
-                    snowflakes.append(contentsOf: (0 ..< (targetCount - snowflakes.count)).map { _ in
-                        Snowflake(
-                            startX: .random(in: 0 ... size.width),
-                            startY: .random(in: -size.height ... 0),
-                            startRotation: .random(in: 0 ... 360),
-                            speed: .random(in: 10 ... 30),
-                            rotationSpeed: .random(in: -25 ... 25),
-                            opacity: .random(in: 0.3 ... 0.4),
-                            size: 5,
-                            noiseSeed: .random(in: 0 ... 1000),
-                            rectCount: .random(in: 2 ... 5),
-                            raysCount: .random(in: 5 ... 7),
-                        )
-                    })
-                }
+            if snowflakes.count > targetCount {
+                snowflakes.removeLast(snowflakes.count - targetCount)
+            } else if snowflakes.count < targetCount {
+                snowflakes.append(contentsOf: (0 ..< (targetCount - snowflakes.count)).map { _ in
+                    Snowflake(
+                        startX: .random(in: 0 ... size.width),
+                        startY: .random(in: -size.height ... 0),
+                        startRotation: .random(in: 0 ... 360),
+                        speed: .random(in: 10 ... 30),
+                        rotationSpeed: .random(in: -25 ... 25),
+                        opacity: .random(in: 0.3 ... 0.4),
+                        size: .random(in: 4 ... 5),
+                        noiseSeed: .random(in: 0 ... 1000),
+                        rectCount: .random(in: 2 ... 5),
+                        raysCount: .random(in: 5 ... 7),
+                    )
+                })
             }
         }
     }
